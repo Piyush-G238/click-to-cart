b31c88cf1f056d34b55f4279e86054a4f2bbe7aa
Piyush Gupta <guptapiyush238@gmail.com>
2023-05-21 17:02:42 +0530

    GatewayService: Configuration for gateway service in microservice project

---
+++ gatewayservice/src/test/java/com/piyush/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
